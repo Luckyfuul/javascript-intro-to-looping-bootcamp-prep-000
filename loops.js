1296c7054f7e9b0d42ba523f8ffde89b80fec3d2
TJ Zuvic <terrance@zuvmarketing.com>
2018-06-03 06:27:02 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++)  {
+    array.push(`I am 1 strange loop.`)
+ 
+  }
+console.log(array)
+return array
+
+}
+function whileLoop(n) {
+  while(n>0){n - 1
+    }
+    console.log("done")
+  }
